21df563b369096dea52b2ca109d9085e70a5ba04
sulayman <sulaymankinteh918@gmail.com>
2026-01-19 13:15:57 +0000

    fixed vercel deployment isuues

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 
 import './App.css';
 import Navbar from './Components/Navbar/Navbar';
-import { BrowserRouter,Routes,Route } from 'react-router-dom';
+import { Routes,Route } from 'react-router-dom';
 import Shop from './Pages/Shop';
 import ShopCategory from './Pages/ShopCategory';
 import Product from './Pages/Product';
@@ -17,7 +17,7 @@ function App() {
   return (
     <div className="App">
       
-      <BrowserRouter>
+     
       <Navbar/>
       <Routes>
         <Route path='/' element={<Shop/>}/>
@@ -33,7 +33,7 @@ function App() {
       </Routes>
       <Footer/>
       
-      </BrowserRouter>
+    
      
     </div>
   );
